01b1995e1690422ebe0e00e78b6b5736fa368aee
Noveedgithub <noveed.muhammed@hotmail.com>
2024-04-28 21:18:23 +0100

    uploading for_loops.py

--- for_loops.py
+++ for_loops.py
@@ -39,3 +39,36 @@ for x in fruits:
 
 for x in range(6):                          # The range has been set to 6. This will now print number upto 5 on a seperate line. 
   print(x)                                  # This is because the (6) refers to index positions and index positions start at 0. (0=1, 1=2, 2=3 etc)
+
+for x in range(2, 6):                       # You can also set your own range by adding the parameter in the brackets.
+  print(x)                                  # eg for x in range(2, 6) will means values from 2 to 6 (not including 6, so will stop at 5)
+  
+for x in range(2, 30, 3):                   # it is possible to specify the increment value by adding a third parameter: range(2, 30, 3)
+  print(x)                                  # This will now start the range from 2 until 30 and will go up in incriments of 3.
+  
+
+# Else in For Loop
+# The else keyword in a for loop specifies a block of code to be executed when the loop is finished
+
+for x in range(6):                          
+  print(x)
+else:                                       # Once the for loops has finished, the else statement is executed and will print "Finally Finished".
+  print("Finally finished!")                # The else statement will not work if the loop is stopped by a break statement 
+  
+
+# NESTED LOOPS - A nested loop is a loop inside a loop
+# The "inner loop" will be executed one time for each iteration of the "outer loop"
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y) 
+    
+    
+# THE PASS STATEMENT
+# for loops cannot be empty, but if you for some reason have a for loop with no content, put in the pass statement to avoid getting an error
+
+for x in [0, 1, 2]:
+  pass
